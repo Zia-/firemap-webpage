4b2baf0cc594891fd0f949f0f17bb35524c21b6d
Mohammed Zia <mohammed.zia33@gmail.com>
2016-06-11 00:52:05 +0300

    working perfect

--- js/main.js
+++ js/main.js
@@ -10,7 +10,7 @@
     }), (new WOW).init(), t("a.page-scroll").bind("click", function(a) {
         var e = t(this);
         t("html, body").stop().animate({
-            scrollTop: t(e.attr("href")).offset().top - 60
+            scrollTop: t(e.attr("href")).offset().top - 100
         }, 1450, "easeInOutExpo"), a.preventDefault()
     }), t(".navbar-collapse ul li a").click(function() {
         t(".navbar-toggle:visible").click()
