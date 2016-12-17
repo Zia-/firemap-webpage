65ef21a1a1763e149f4ce83de2e01702322e7bec
Mohammed Zia <mohammed.zia33@gmail.com>
2016-12-17 15:37:20 +0200

    near realtime also added

---
+++ nearrealtime/js/firmsModis.js
@@ -0,0 +1,84 @@
+// Load modis c6 data --------------------------------------------
+var firmsModisc6Realurl = "http://maps.itu.edu.tr/maps/realdata-geojson/modis_c6/MODIS_C6_Global_24h_turkeyFiltered.geojson"
+
+// Create modis c6 featureLayer
+var firmsModisc6FeatureLayer = L.mapbox.featureLayer(firmsModisc6Realurl, {
+    pointToLayer: function(feature, latlng) {
+        var lng = latlng['lng']
+        var lat = latlng['lat']
+        return L.polygon([L.latLng(lat,lng-0.02548), L.latLng(lat+0.02548,lng), L.latLng(lat,lng+0.02548), L.latLng(lat-0.02548,lng), L.latLng(lat,lng-0.02548)]);
+    },
+    style: function (feature) {
+      var rgb = '#58ACFA'
+      return {color: rgb, weight: 1, opacity: 1};
+    },
+})
+  .on('ready', featureLayerModisc6PopupBind)
+
+// Bind popup to modis c6 layer features
+function featureLayerModisc6PopupBind() {
+  firmsModisc6FeatureLayer.eachLayer(function(layer) {
+    var popup = new L.Popup({ autoPan: false });
+    var baseUrl = "https://maps.google.com/?q=";
+    var googleMapsUrl = baseUrl.concat(layer.feature.geometry.coordinates[1],
+                                      ",",layer.feature.geometry.coordinates[0],
+                                      "&ll=",layer.feature.geometry.coordinates[1],
+                                      ",",layer.feature.geometry.coordinates[0],
+                                      "&z=10");
+    popup.setContent(
+      '<div class="table-responsive">    '+
+          '<table class = "table">'+
+       '<caption><h1><strong>Fire Radiative Power:</strong> ' + Math.round(layer.feature.properties.FIRE_RADIATIVE_POWER) + ' MW</h1></caption>'+
+       '<tbody>'+
+        '  <tr class = "danger">'+
+            ' <td><button type="button" class="btn-danger" onclick="window.open(\'' + googleMapsUrl + '\')">Location</button></td>'+
+            ' <td>Lat: ' + layer.feature.geometry.coordinates[1] + ', Lon: ' + layer.feature.geometry.coordinates[0] + '</td>'+
+        '  </tr>'+
+        '  <tr class = "warning">'+
+          '   <td>Time</td>'+
+          '   <td>' + layer.feature.properties.ACQUISITION_TIME + '</td>'+
+          '</tr>'+
+          '<tr>'+
+          '   <td>Brightness</td>'+
+          '   <td>' + layer.feature.properties.BRIGHTNESS + ' K</td>'+
+        '  </tr>'+
+        '  <tr class = "active">'+
+        '     <td>Brightness T Ch31</td>'+
+          '   <td>' + layer.feature.properties.BRIGHTNESS_TEMPERATURE_CHANNEL31 + ' K</td>'+
+        '  </tr>'+
+        '  <tr class = "active">'+
+        '     <td>Date</td>'+
+          '   <td>' + layer.feature.properties.ACQUISITION_DATE + '</td>'+
+        '  </tr>'+
+        '  <tr class = "active">'+
+        '     <td>Day / Night</td>'+
+          '   <td>' + layer.feature.properties.DAYNIGHT + '</td>'+
+        '  </tr>'+
+       '</tbody>'+
+      '</table>'+
+      '</div>'
+    )
+    layer.bindPopup(popup);
+  });
+}
+
+// Below will be handled by modis c6 click buttons
+var firmsModisc6ToggleVar = 0;
+var firmsModisc6Timer;
+function firmsModisc6() {
+  if (firmsModisc6ToggleVar == 0){
+    // Load modis c6 data
+    firmsModisc6FeatureLayer.addTo(map);
+    // Start timer
+    firmsModisc6Timer = setInterval(function(){firmsModisc6FeatureLayer.loadURL(firmsModisc6Realurl);}, 300000);
+    // Change toggler value
+    firmsModisc6ToggleVar = 1;
+  } else {
+    // Unload modis c6 data
+    map.removeLayer(firmsModisc6FeatureLayer);
+    // Stop timer
+    clearInterval(firmsModisc6Timer);
+    // Change toggler value
+    firmsModisc6ToggleVar = 0;
+  }
+}
---
+++ nearrealtime/js/frpPixelSeviri.js
@@ -0,0 +1,84 @@
+// Load frp pixel seviri data ---------------------------------------------
+var frpPixelSeviriRealurl = "http://maps.itu.edu.tr/maps/realdata-geojson/seviri_lsa/HDF5_LSASAF_MSG_FRP-PIXEL-ListProduct_MSG-Disk_turkeyFiltered.geojson"
+
+// Create frp pixel seviri featureLayer
+var frpPixelSeviriFeatureLayer = L.mapbox.featureLayer(frpPixelSeviriRealurl, {
+    pointToLayer: function(feature, latlng) {
+        var lng = latlng['lng']
+        var lat = latlng['lat']
+        return L.polygon([L.latLng(lat,lng-0.02548), L.latLng(lat+0.02548,lng), L.latLng(lat,lng+0.02548), L.latLng(lat-0.02548,lng), L.latLng(lat,lng-0.02548)]);
+    },
+    style: function (feature) {
+      var rgb = '#00FF00'
+      return {color: rgb, weight: 1, opacity: 1};
+    },
+})
+  .on('ready', featureLayerFrpPixelSeviriPopupBind)
+
+// Bind popup to frp pixel seviri layer features
+function featureLayerFrpPixelSeviriPopupBind() {
+  frpPixelSeviriFeatureLayer.eachLayer(function(layer) {
+    var popup = new L.Popup({ autoPan: false });
+    var baseUrl = "https://maps.google.com/?q=";
+    var googleMapsUrl = baseUrl.concat(layer.feature.geometry.coordinates[1],
+                                      ",",layer.feature.geometry.coordinates[0],
+                                      "&ll=",layer.feature.geometry.coordinates[1],
+                                      ",",layer.feature.geometry.coordinates[0],
+                                      "&z=10");
+    popup.setContent(
+      '<div class="table-responsive">    '+
+          '<table class = "table">'+
+       '<caption><h1><strong>Fire Radiative Power:</strong> ' + Math.round(layer.feature.properties.FIRE_RADIATIVE_POWER) + ' MW</h1></caption>'+
+       '<tbody>'+
+        '  <tr class = "danger">'+
+            ' <td><button type="button" class="btn-danger" onclick="window.open(\'' + googleMapsUrl + '\')">Location</button></td>'+
+            ' <td>Lat: ' + layer.feature.geometry.coordinates[1] + ', Lon: ' + layer.feature.geometry.coordinates[0] + '</td>'+
+        '  </tr>'+
+        '  <tr class = "success">'+
+        '     <td>Pixel Size</td>'+
+          '   <td>' + layer.feature.properties.PIXEL_SIZE + '</td>'+
+        '  </tr>'+
+        '  <tr class = "warning">'+
+          '   <td>Time</td>'+
+          '   <td>' + layer.feature.properties.ACQUISITION_TIME + '</td>'+
+          '</tr>'+
+          '<tr>'+
+          '   <td>Brightness T MIR</td>'+
+          '   <td>' + layer.feature.properties.BRIGHTNESS_TEMPERATURE_MIR + ' K</td>'+
+        '  </tr>'+
+        '  <tr class = "active">'+
+        '     <td>Brightness T TIR</td>'+
+          '   <td>' + layer.feature.properties.BRIGHTNESS_TEMPERATURE_TIR + ' K</td>'+
+        '  </tr>'+
+        '  <tr class = "active">'+
+        '     <td>Fire Confidence</td>'+
+          '   <td>' + layer.feature.properties.FIRE_CONFIDENCE + '</td>'+
+        '  </tr>'+
+       '</tbody>'+
+      '</table>'+
+      '</div>'
+    )
+    layer.bindPopup(popup);
+  });
+}
+
+// Below will be handled by frpPixelSeviri click buttons
+var frpPixelSeviriToggleVar = 0;
+var frpPixelSeviriTimer;
+function frpPixelSeviri() {
+  if (frpPixelSeviriToggleVar == 0){
+    // Load frp pixel seviri data
+    frpPixelSeviriFeatureLayer.addTo(map);
+    // Start timer
+    frpPixelSeviriTimer = setInterval(function(){frpPixelSeviriFeatureLayer.loadURL(frpPixelSeviriRealurl);}, 300000);
+    // Change toggler value
+    frpPixelSeviriToggleVar = 1;
+  } else {
+    // Unload frp pixel seviri data
+    map.removeLayer(frpPixelSeviriFeatureLayer);
+    // Stop timer
+    clearInterval(frpPixelSeviriTimer);
+    // Change toggler value
+    frpPixelSeviriToggleVar = 0;
+  }
+}
